5a8f4c110645c7365e259bdb938901a23d26102a
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-12-17 19:36:47 +0000

    initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+app.use('/api', graphqlHTTP({
+    //schema,
+    graphiql: true,
+}));
+
+app.listen(4000, () => console.log('server is running...'));
